c5a519fb01fe20383fda61eef6e3f8176bf0d1ba
mrinalsinha2211 <mrinalsinha53@gmail.com>
2024-01-09 21:48:17 +0530

    changed

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -10,6 +10,13 @@ router.post('/users', async (req, res) => {
   // 2. Create a new user using User.create()
   // 3. Handle success: Respond with a 201 status code and the created user
   // 4. Handle errors: Respond with appropriate error messages and status codes
+  const{name , email }=req.body;
+  try {
+    const newUser=await User.create({name ,email});
+    res.status(201).json({message: "User created", user:newUser});
+  } catch (error) {
+    res.status(500).json({message: "Internal Server Error"});
+  }
 });
 
 // Retrieve a user by ID
@@ -19,6 +26,17 @@ router.get('/users/:id', async (req, res) => {
   // 2. Find the user by ID using User.findById()
   // 3. Handle success: Respond with a 200 status code and the user data
   // 4. Handle errors: Respond with appropriate error messages and status codes
+  const userId = req.params.id;
+  try {
+    const user=await  User.findById({userId});
+    if(user)
+    {res.status(200).json({user});
+  }else{
+    res.status(404).json({message : "User not found"});
+  }
+  } catch (error) {
+    res.status(500).json({message: "Internal Server Error"});
+  }
 });
 
 // Update a user by ID
@@ -29,6 +47,18 @@ router.patch('/users/:id', async (req, res) => {
   // 3. Use User.findByIdAndUpdate() to update the user
   // 4. Handle success: Respond with a 200 status code and the updated user data
   // 5. Handle errors: Respond with appropriate error messages and status codes
+  const userId = req.params.id;
+  const updateBody=req.body;
+  try {
+    const user=await  User.findByIdAndUpdate({userId , updateBody});
+    if(user)
+    {res.status(200).json({message: "User updated", user});
+  }else{
+    res.status(404).json({message : "User not found"});
+  }
+  } catch (error) {
+    res.status(500).json({message: "Internal Server Error"});
+  }
 });
 
 // Delete a user by ID
@@ -38,6 +68,17 @@ router.delete('/users/:id', async (req, res) => {
   // 2. Use User.findByIdAndDelete() to delete the user
   // 3. Handle success: Respond with a 200 status code and a deletion confirmation message
   // 4. Handle errors: Respond with appropriate error messages and status codes
+  const userId = req.params.id;
+  try {
+    const user=await  User.findByIdAndDelete({userId});
+    if(user)
+    {res.status(200).json({message: "User deleted"});
+  }else{
+    res.status(404).json({message : "User not found"});
+  }
+  } catch (error) {
+    res.status(500).json({message: "Internal Server Error"});
+  }
 });
 
 module.exports = router;
--- model/userModel.js
+++ model/userModel.js
@@ -5,6 +5,9 @@ const userSchema = new mongoose.Schema({
   // Implement the user schema fields:
   // name as String and
   // email as a String,
+  name: String,
+  email: String,
 });
 
 // Create and export the User model
+module.exports = mongoose.Model("User",userSchema);
